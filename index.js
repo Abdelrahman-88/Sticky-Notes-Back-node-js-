34663fb781b9b11e08da262fc456f800d70e9bd6
Abdelrahman-88 <abdo.elsaid@hotmail.com>
2022-04-22 13:56:08 +0200

    make it better

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ const app = express()
 const port = process.env.PORT || 5000
 app.use(express.json())
 app.use(cors())
+app.get('/', (req, res) => res.send('welcome'))
+
 app.use(userRouter, noteRouter, paymentRoutes)
 dbConnection()
 
